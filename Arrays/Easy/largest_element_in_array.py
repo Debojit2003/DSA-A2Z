8614bd3b3486ffffbebc1eb79f16cb2b6bae5b84
Soumili Lahiri <155342372+Mili-sou@users.noreply.github.com>
2025-04-13 12:21:40 +0530

    Update largest_element_in_array.py

--- Arrays/Easy/largest_element_in_array.py
+++ Arrays/Easy/largest_element_in_array.py
@@ -1,5 +1,13 @@
+#approach1
 def largest(arr):
     return max(arr)
 
 arr = list(map(int,input("Enter the array elements comma seperated: ").split()))
 print("Largest element is", largest(arr))
+
+#approach2
+def largest(arr):
+    arr.sort()
+    return arr[-1]
+arr=list(map(int,input("Enter the elements comma seperated:").split()))
+print("Largest element is:",largest(arr))
